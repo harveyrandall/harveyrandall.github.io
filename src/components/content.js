91100e9b286361872993b2b58a9dde40272c32dd
Harvey Randall <harvey_randall@me.com>
2019-02-10 23:42:16 +0000

    updated blog to appearance wanted

---
+++ src/components/content.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { profile, education, projects, experience } from '../profile.json';
+
+class Content extends Component {
+	render() {
+
+		return (
+			<div className={this.props.className}>
+				<Section sectionName="Profile">
+					{profile}
+				</Section>
+				<Section sectionName="Education">
+					{education.map((val, index) => {
+						return (
+							<Item key={index} details={val}>
+								<Education qualifications={val.qualifications} />
+							</Item>
+						);
+					})}
+				</Section>
+				<Section sectionName="Projects">
+					{projects.map((val, index) => {
+						return (
+							<Item key={index} details={val} />
+						)
+					})}
+				</Section>
+				<Section sectionName="Experience">
+					{experience.map((val, index) => {
+						return <Item key={index} details={val} />
+					})}
+				</Section>
+			</div>
+		);
+	}
+}
+
+const Section = ({children, sectionName}) => {
+	return (
+		<div className="section">
+			<div className="section-name">
+				{sectionName}
+			</div>
+			<div className="section-content">
+				{children}
+			</div>
+		</div>
+	);
+}
+
+const Item = ({children, details}) => {
+	const heading = details.url ?
+					<h6><a href={details.url}>{details.name}</a></h6> :
+					<h6>{details.name}</h6>;
+
+	let dateRange = undefined;
+	if(details.start_date || details.end_date) {
+		const startDate = formatDate(details.start_date);
+		const endDate = formatDate(details.end_date);
+		dateRange = <p>{startDate} - {endDate}</p>;
+	}
+	const description = details.description ? <div className="item-description">{details.description}</div> : undefined;
+
+	return (
+		<div className="item">
+			<div className="item-details">
+				{heading}
+				<p className="position">{details.position}</p>
+				{dateRange}
+			</div>
+			{description}
+			<div className="item-data">
+				{children}
+			</div>
+		</div>
+	);
+}
+
+const Education = ({qualifications}) => {
+	const type = Object.keys(qualifications);
+	return (
+		<div className="qualifications">
+			{type.map((val, index) => {
+				return (
+					<div key={index} className="qualification">
+						<h6 className="qualification-type">{val}</h6>
+						<Qualifications data={qualifications[val]} />
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+const Qualifications = ({data}) => {
+	return (
+		<ul className="qualification-list">
+			{data.map((val, index) => {
+				return <li key={index}><b>{val.subject}</b> - {val.grade}</li>;
+			})}
+		</ul>
+	);
+}
+
+const formatDate = (date) => {
+	if(date) {
+		let options = {month: 'long', year: 'numeric'};
+		return new Date(date).toLocaleDateString('en-GB', options);
+	}
+	return "Present";
+}
+
+
+export default Content;
---
+++ src/components/sidebar.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { name, description, location, contact } from "../profile.json";
+
+import personalImage from "../images/sideImage.jpg";
+
+class Sidebar extends Component {
+	render() {
+		return (
+			<div className={this.props.className}>
+				<div className="side-info">
+					<Image
+						url={personalImage}
+						alt={name}
+					/>
+					<Title
+						name={name}
+					/>
+					<Description
+						className="description"
+						text={description}
+					/>
+					<Separator />
+					<Footer icons={contact}/>
+				</div>
+			</div>
+		)
+	}
+}
+
+const Separator = () => {
+	return (
+		<div className="sep"></div>
+	);
+}
+
+const Title = ({className, name}) => {
+	return (
+		<h2>{name}</h2>
+	);
+}
+
+const Image = ({className, url, alt}) => {
+	return (
+		<img src={url} alt={alt} />
+	);
+}
+
+const Description = ({className, text}) => {
+	return (
+		<div className={className}>
+			<p>{text}</p>
+			<Separator />
+			<small className="location">
+				<Icon name="fas fa-map-marker-alt" />
+				{location}
+			</small>
+		</div>
+	);
+}
+
+const Footer = ({icons}) => {
+	return (
+		<div className="side-footer">
+			{contact.map(function(val){
+				return <Icon key={val.platform} url={val.url} name={val.classes} />
+			})}
+		</div>
+	);
+}
+
+const Icon = ({url, name}) => {
+	return (
+		<a href={url}
+			className="contact-icon"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<i className={name}></i>
+		</a>
+	);
+}
+
+
+export default Sidebar;
